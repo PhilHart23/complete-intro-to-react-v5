54caa88b8e0b765615c2823f4657150ac60a7a75
Phil Hart <philhart23@gmail.com>
2019-12-04 11:12:54 -0700

    Revert "convert project to JSX, add React-specific ESLint config"
    This reverts commit 0f7977ae3ce923ecf7094b82056a809fcaf1e905.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  const theme = useState("darkblue");
-  return (
-    <ThemeContext.Provider value={theme}>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
-    </ThemeContext.Provider>
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
+  ]);
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
